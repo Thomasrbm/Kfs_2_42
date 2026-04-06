b0d781f02dcbd32b3ecb34aa930f69203c9f3f59
Thomasrbm <frozyxyt.76@gmail.com>
2026-04-06 02:53:53 +0200

    fix: fixing help printing

--- srcs/cmds/help.c
+++ srcs/cmds/help.c
@@ -3,12 +3,14 @@
 
 void    help_cmd()
 {
+    g_cursor_col[g_screen] = g_prompt_col;
     printk("Available commands:\n", MAGENTA_ON_BLACK);
     printk("help     - show this help\n", LIGHT_MAGENTA_ON_BLACK);
     printk("clear    - clear the screen\n", LIGHT_MAGENTA_ON_BLACK);
     printk("reboot   - reboot the machine\n", LIGHT_MAGENTA_ON_BLACK);
     printk("halt     - halt the CPU\n", LIGHT_MAGENTA_ON_BLACK);
     printk("poweroff - power off the machine\n", LIGHT_MAGENTA_ON_BLACK);
+    print_prompt();
 }
 
 
